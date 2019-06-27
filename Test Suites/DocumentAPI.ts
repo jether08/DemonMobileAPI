576aef9e4881ea095c8b537a9474fd5f7fe6f372
arcillas <arcillas@arcillas-PC>
2019-06-27 16:23:37 +0800

    Adding new test suites for DocumentAPI

---
+++ Test Suites/DocumentAPI.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test suites for DocumentAPI</description>
+   <name>DocumentAPI</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>02794717-d50b-43b2-a16a-9b3c1e3ca252</testSuiteGuid>
+   <testCaseLink>
+      <guid>eb822e8e-147b-4c64-bb4e-0ecbc1da9f56</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DocumentAPI/ValidateCreateDocument</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
